0caff791eac193a4301556c782adeb91dd20e377
sharath V <sharathkrml@gmail.com>
2022-02-25 15:18:44 +0530

    added NFT contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    string _baseTokenURI;
+    uint256 public _price = 0.01 ether;
+    uint256 tokenIds;
+
+    constructor(string memory baseURI) ERC721("MyNFT", "MFT") {
+        _baseTokenURI = baseURI;
+    }
+
+    function mint() public payable {
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+}
